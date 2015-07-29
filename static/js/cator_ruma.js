8432488f67e5ee622788d604e9e4f94ace3766f9
Drew Lazzeri <dlazzeri1@gmail.com>
2015-07-29 14:29:31 -0600

    Added scrollTo functionality
    Updated pip

--- static/js/cator_ruma.js
+++ static/js/cator_ruma.js
@@ -1,11 +1,13 @@
-$(window).on("scroll", function() {
+document.addEventListener("touchmove", resize, false);
+document.addEventListener("scroll", resize, false);
+
+function resize() {
     var s = Math.min(60, Math.max(25, 60-$(document).scrollTop()/2));
     $("#logo").width(s).height(s);
-
 
     var t = $(document).scrollTop()/6;  //half of percentage scrolled in pixels
     $("header").css({
         'margin-left': 21 - Math.min(12, t) + '%',
         'width': Math.min(79 + t, 91) + '%'
     });
-});
+}
