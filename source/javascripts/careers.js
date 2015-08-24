4169826ab399852dfea4b632303be01c84ea1047
Drew Lazzeri <dlazzeri1@gmail.com>
2015-08-24 15:57:34 -0600

    Careers page has templates too

---
+++ source/javascripts/careers.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    var feature = $('#feature').html();
+    var feature_template = Handlebars.compile(feature);
+    
+    var jobs = $('#jobs').html();
+    var jobs_template = Handlebars.compile(jobs);
+
+    function use_backup() {
+    };
+
+    $.get('careers.yaml')
+        .done(function(data) {
+            try{
+                var yaml = jsyaml.load(data);
+                
+                for (var i=0; i < yaml.jobs.length; i++) {
+                    yaml.jobs[i].safetitle = class_safe(yaml.jobs[i].title)
+                }
+                $('#feature-placeholder').html(feature_template(yaml));
+                $('#jobs-placeholder').html(jobs_template(yaml));
+            }
+            catch (e) {
+                console.log("Yaml is ill-formed", e);
+                use_backup();
+            }
+        }).fail(use_backup);
+});
