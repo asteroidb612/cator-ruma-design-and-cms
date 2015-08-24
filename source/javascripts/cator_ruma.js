39d0bb18200d204129159f51c954ecdf814e1e3a
Drew Lazzeri <dlazzeri1@gmail.com>
2015-08-24 14:29:08 -0600

    Contact's have been yaml-handlebared

--- source/javascripts/cator_ruma.js
+++ source/javascripts/cator_ruma.js
@@ -21,4 +21,31 @@ window.addEventListener("resize", resize, false);
 
 $(document).ready(function () {
     resize();
+    
+    var contact = $('#contact').html();
+    var contact_template = Handlebars.compile(contact);
+    
+    function use_backup() {
+    }
+
+    $.get('contact.yaml')
+        .done(function(data) {
+            try{ 
+                var yaml = jsyaml.load(data);
+                
+                for(var i=0; i<yaml.locations.length; i++) {
+                    var daddr = yaml.locations[i].line1 +' '+ yaml.locations[i].line2
+                    daddr = daddr.replace(/\W+/g,'+');
+                    yaml.locations[i].daddr = daddr;
+                    
+                    var phone_link = '+1' + yaml.locations[i].phone.replace(/\W+/g, '');
+                    yaml.locations[i].phone_link = phone_link;
+                } 
+                $('#contact-placeholder').html(contact_template(yaml));
+            }
+            catch (e) {
+                console.log("Yaml is ill-formed", e);
+                use_backup();
+            }
+        }).fail(use_backup);
 });
